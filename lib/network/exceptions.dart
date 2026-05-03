57617687389a03c02a2d484c66aaacc162d981e1
Ghaffar <ghaffarbwp786@gmail.com>
2026-05-04 03:46:39 +0500

    Show clear signup errors

--- lib/network/exceptions.dart
+++ lib/network/exceptions.dart
@@ -1,22 +1,29 @@
- abstract class AppExceptions implements Exception{
- final String msg;
- final String? details;
- AppExceptions(this.msg,this.details);
+abstract class AppExceptions implements Exception {
+  final String msg;
+  final String? details;
 
+  AppExceptions(this.msg, this.details);
 
- }
- class FetchDataExceptions extends AppExceptions {
-  FetchDataExceptions([String? msg]) : super(msg!, '');
-
- }
- class BadRequestException  extends AppExceptions{
- BadRequestException ([String? msg]) : super (msg!, 'Invalid Request: ');
-}
- class UnauthorizedException  extends AppExceptions{
- UnauthorizedException ([String? msg]) : super (msg!, 'Unauthorized: ');
-}
- class InvalidInputException  extends AppExceptions{
- InvalidInputException ([String? msg]) : super (msg!, 'Invalid Input:');
+  @override
+  String toString() {
+    final prefix = details?.trim() ?? '';
+    if (prefix.isEmpty) return msg;
+    return '$prefix $msg'.trim();
+  }
 }
 
+class FetchDataExceptions extends AppExceptions {
+  FetchDataExceptions([String? msg]) : super(msg ?? 'Something went wrong', '');
+}
 
+class BadRequestException extends AppExceptions {
+  BadRequestException([String? msg]) : super(msg ?? 'Invalid request', '');
+}
+
+class UnauthorizedException extends AppExceptions {
+  UnauthorizedException([String? msg]) : super(msg ?? 'Unauthorized', '');
+}
+
+class InvalidInputException extends AppExceptions {
+  InvalidInputException([String? msg]) : super(msg ?? 'Invalid input', '');
+}
